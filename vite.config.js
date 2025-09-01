2fc61beb9701ae05fc79a9916dc94e2eb812ddad
Thanadonsaer <143170036+Thanadonsaer@users.noreply.github.com>
2025-09-01 18:18:38 +0700

    Initial commit - Thai ID Card Reader

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/thanadonsaer/thai-id-card-reader/',
+  base: '/thai-id-card-reader/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
